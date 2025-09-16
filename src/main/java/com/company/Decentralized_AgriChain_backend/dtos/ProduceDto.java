183bc39074d7b38f5366f8159c7bf79206d6e0b1
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-16 13:38:08 +0530

    created the service interface for the Actor and Produce

--- src/main/java/com/company/Decentralized_AgriChain_backend/dtos/ProduceDto.java
+++ src/main/java/com/company/Decentralized_AgriChain_backend/dtos/ProduceDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ProductDto {
+public class ProduceDto {
 
     private Long id;
     private String name;
---
+++ src/main/java/com/company/Decentralized_AgriChain_backend/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.company.Decentralized_AgriChain_backend.service;
+
+import com.company.Decentralized_AgriChain_backend.dtos.ProduceDto;
+import com.company.Decentralized_AgriChain_backend.dtos.TransferProduceDto;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProduceDto addProduce(ProduceDto produceDto);
+    ProduceDto getProduceById(Long id);
+    List<ProduceDto> getProduceByOwner(Long actorId);
+    ProduceDto transferProduce(TransferProduceDto transferProduceDto);
+}
